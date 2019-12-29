350dabcd67b7cae31c33bb48e0e10c5201c372be
drefrant <diensa.refranto@gmail.com>
2019-12-30 04:15:47 +0700

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+console.log('Client side javascript file is loaded!')
+
+// Promises
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-icon')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    messageThree.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+                messageThree.innerHTML = '<img width="84" height="84" src="https://darksky.net/images/weather-icons/' + data.icon + '.png">' + Math.round(data.temperatures) + '˚&nbsp;'
+            }
+        })
+    })   
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,26 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url ='https://api.darksky.net/forecast/d16c781904db61bf6c66d79e2367b13e/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '?units=si&lang=id'
+
+    request({ url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            //callback(undefined, body.daily.data[0].summary + ' Saat ini ' + body.currently.temperature + ' derajat celcius. Kemungkinan ' + body.currently.precipProbability + '% hujan.')
+            callback(undefined, {
+                latitude: body.latitude,
+                longitude: body.longitude,
+                timezone: body.timezone,
+                summary: body.daily.data[0].summary + ' Saat ini ' + body.currently.temperature + ' derajat celcius. Kemungkinan ' + body.currently.precipProbability + '% hujan.',
+                temperature: body.currently.temperature,
+                precip: body.currently.precipProbability,
+                icons: body.currently.icon
+            })
+        }
+    })
+}
+
+module.exports = forecast
